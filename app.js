8cbf2cf8605a8bb33d4fb8779249c02bdc370186
Jh-Arman <sarmani2004@gmail.com>
2025-09-05 21:24:27 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+const body = document.getElementById('body');
+const button = document.querySelectorAll('li');
+
+//const redColor = document.querySelector('.red');
+//redColor.addEventListener('click',function(){
+  //  body.style.backgroundColor = '#ff7675';
+//} )
+
+//const greenColor = document.querySelector('.green');
+//greenColor.addEventListener('click',function(){
+  //  body.style.backgroundColor = '#55efc4';
+//} );
+
+button.forEach(function(value){
+ value.addEventListener('click',function(){
+    let className = this.classList[0];
+    let color = '';
+    if(className=== 'red'){
+      color = '#ff7675'
+    }
+    if(className=== 'green'){
+      color = '#55efc4'
+    }
+    if(className=== 'blue'){
+      color = '#74b9ff'
+    }
+    if(className=== 'pink'){
+      color = '#fd79a8'
+    }
+    body.style.backgroundColor = color;
+ })
+})
